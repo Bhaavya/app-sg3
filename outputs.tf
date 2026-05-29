1770414bd12f22fbb8b647c1ad5c535e90726c26
Bhaavya <bhavya2@illinois.edu>
2026-05-29 04:12:14 +0000

    Add terraform configuration for security group with no tags

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "security_group_id" {
-  value = aws_security_group.this.id
+  description = "The ID of the security group"
+  value       = aws_security_group.this.id
 }
 
 output "security_group_arn" {
-  value = aws_security_group.this.arn
+  description = "The ARN of the security group"
+  value       = aws_security_group.this.arn
 }
 
 output "security_group_name" {
-  value = aws_security_group.this.name
+  description = "The name of the security group"
+  value       = aws_security_group.this.name
 }
 
 output "region" {
-  value = var.region
+  description = "The AWS region"
+  value       = var.region
 }
